c33b22da16ebe4236d2ca148e90082aa8e5a9d44
Tobias Mesquita <tobias.mesquita@gmail.com>
2022-10-07 09:55:32 -0300

    initial commit

---
+++ ui/dev/src/pages/Test1.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page padding>
+    
+    
+  </q-page>
+</template>
+
+<style lang="sass" scoped>
+.directive-target
+  width: 50px
+  height: 50px
+</style>
---
+++ ui/src/vue-plugin.js
@@ -0,0 +1,18 @@
+
+
+
+const version = __UI_VERSION__
+
+function install (app) {
+
+}
+
+function sayHello (name) {
+  console.log('hello ' + name)
+}
+
+export {
+  version,
+  sayHello,
+  install
+}
